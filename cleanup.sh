6507ef76b896c10634b058da2867fddae6c8344e
Necr001 <bek.nurekeev02@gmail.com>
2025-06-25 17:17:48 +0600

    Fixed in run.sh and added check.sh and cleanup.sh files

--- cleanup.sh
+++ cleanup.sh
@@ -1,12 +1,6 @@
-#!/bin/bash
-
 # Set image, container and port
 IMAGE_NAME="ascii-art-web-dockerize"
 CONTAINER_NAME="ascii-art-web"
-PORT=7777:7777
-
-echo "🔨 Building Docker image..."
-docker image build -f Dockerfile -t $IMAGE_NAME .
 
 # Stop and remove old container if it exists
 if docker ps -a --format '{{.Names}}' | grep -Eq "^$CONTAINER_NAME\$"; then
@@ -16,8 +10,5 @@ if docker ps -a --format '{{.Names}}' | grep -Eq "^$CONTAINER_NAME\$"; then
   docker container rm $CONTAINER_NAME
 fi
 
-# Build or rebuild container
-echo "🚀 Running Docker container..."
-docker container run -p $PORT --detach --name $CONTAINER_NAME $IMAGE_NAME
-
-echo "🌐 App is now running at http://localhost:7777"
+echo "🗑️ Removing image..."
+docker image rm $IMAGE_NAME
